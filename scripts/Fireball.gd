4703281652bdecc67a87baf6c6eea4d1063bd469
Dan D. <daniel.mi.do@gmail.com>
2024-01-28 09:17:12 +0000

    Enemy is no longer dragged with the player Fireballs disappear on collision Fixed projectile raycast bug

--- scripts/Fireball.gd
+++ scripts/Fireball.gd
@@ -6,6 +6,7 @@ class_name Fireball extends Projectile
 var destroyed : bool = false
 @onready var collision_timer : Timer = $CollisionDespawnTimer
 @onready var ray : RayCast2D = $Ray
+@onready var sprite : Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,14 +15,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	super(delta)
 	ray.target_position = position + velocity * direction # Replace with function body.
-	if ray.is_colliding():
-		if (ray.get_collision_point() - position).length() < 0.3:
-			collide()
+	if not destroyed:
+		if ray.is_colliding():
+			if (ray.get_collision_point() - position).length() < 5:
+				collide()
 		
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	collide()
 
 func collide() -> void:
+	sprite.hide()
 	$GPUParticles2D.emitting = true
 	velocity = 0
 	destroyed = true
