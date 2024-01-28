f2717c72970c98ec8e91aff4a9a91cc2acd3c2a4
Dan D. <daniel.mi.do@gmail.com>
2024-01-28 10:01:36 +0000

    Opening one chest no longer makes all of them flash

--- scripts/Chest.gd
+++ scripts/Chest.gd
@@ -4,7 +4,7 @@ var open : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimatedSprite2D.material = $AnimatedSprite2D.material.duplicate()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
