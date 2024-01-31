e16d68012f11e668ff58c781b57d91c130a88a31
Dan D. <daniel.mi.do@gmail.com>
2024-01-31 12:16:00 +0000

    Using tweens to fade health bars

--- scripts/entity_bar.gd
+++ scripts/entity_bar.gd
@@ -4,26 +4,28 @@ enum bar_types {red, yellow}
 
 
 @export var type : bar_types = bar_types.red
-@export var show_duration : float = 0.5
-@onready var frames : AnimatedSprite2D = $AnimatedSprite2D
+@export var show_duration : float = 1
+@export var fade_duration : float = 0.3
 
-var time_to_show : float
+@onready var opacity_tween : Tween = get_tree().create_tween()
+@onready var frames : AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	frames.play(str(type))
 	frames.pause()
 	frames.frame = 5
-	hide()
+	frames.modulate.a = 0.0
 
 func update_bar(value : float, max_value : float, difference : float) -> void:
-	show()
-	time_to_show = show_duration
-	frames.frame = round((value / max_value) * 6) 
+	frames.modulate.a = 1.0
+	opacity_tween.kill()
+	opacity_tween = get_tree().create_tween()
+	opacity_tween.tween_interval(show_duration)
+	opacity_tween.tween_property(frames, "modulate:a", 0.0, fade_duration)
+	opacity_tween.play()
+	frames.frame = round((value / max_value) * 6)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if time_to_show > 0:
-		time_to_show -= delta
-		if time_to_show <= 0:
-			hide()
+	pass
