7f937e368212853d55ade4c7a21f437af303132d
Dan D. <daniel.mi.do@gmail.com>
2024-02-07 09:54:05 +0000

    Pathfinding! Navmeshes on tilesets are very restrictive though

--- scripts/basic_movement_controller.gd
+++ scripts/basic_movement_controller.gd
@@ -5,8 +5,10 @@ class_name ExplorerAI extends EnemyAI
 @onready var avoidance_vision : AvoidanceVision = $AvoidanceVision
 @onready var state_chart : StateChart = $StateChart
 @onready var state_label : Label = $StateInformation
+@onready var navigation_agent : NavigationAgent2D = $NavigationAgent2D
 
 var walk_destination : Vector2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,11 +16,23 @@ func _ready() -> void:
 
 func _on_following_state_physics_processing(delta: float) -> void:
 	if not parent.has_los_to(player.global_position):
-		state_chart.send_event("lost sight")
-	# weigh our previous target destination (which was influenced by the helper) with the suggestion
-	parent.target_direction = 0.5 * (player.global_position - parent.global_position).normalized()\
+		pass # state_chart.send_event("lost sight")
+
+	# Navigation code
+	navigation_agent.target_position = get_node("/root/Globals").player.global_position
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = parent.global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	var navigation_direction : Vector2 = current_agent_position.direction_to(next_path_position)
+	
+	# Working out where to actually go
+	parent.target_direction = 0.5 * navigation_direction\
 					+ 0.5 * avoidance_vision.sum
 	parent.velocity += parent.target_direction.normalized() * parent.acceleration * delta
+	
 
 func _on_standing_state_physics_processing(delta: float) -> void:
 	pass
