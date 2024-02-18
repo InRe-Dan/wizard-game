6ef57887fe6c3fd31a82dcb46c901ae1249d501f
Dan D. <daniel.mi.do@gmail.com>
2024-02-18 14:01:40 +0000

    More attempt to fix jitter

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,12 +1,23 @@
 extends Camera2D
+# Code adapted from https://www.reddit.com/r/godot/comments/eu5eyv/sprite_jittering/
+# More potential solutions https://github.com/godotengine/godot/issues/35606
+# r/godot, u/forbjok
 
-@onready var target : Entity = get_tree().get_first_node_in_group("players") as Entity
+# Smoothing duration in seconds
+var SMOOTHING_DURATION : float = 0.5
+# Current position of the camera
+var current_position: Vector2
+# Position the camera is moving towards
+var destination_position: Vector2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	current_position = global_position
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
-	return
+func _process(delta: float) -> void:
+	var target: Node2D = get_tree().get_first_node_in_group("players") as Node2D
+	if target:
+		destination_position = target.global_position
+	current_position += Vector2(destination_position.x - current_position.x, destination_position.y - current_position.y) / SMOOTHING_DURATION * delta
+	
+	# global_position = current_position.round()
+	# force_update_scroll()
