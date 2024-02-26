b6093856a5c3caecd5bd49d25515186976a93e97
Dan D. <daniel.mi.do@gmail.com>
2024-02-26 16:26:25 +0000

    Added dash cooldown

--- entities/player_entity.gd
+++ entities/player_entity.gd
@@ -2,6 +2,11 @@ extends Entity
 
 var hit_sound : AudioStream = preload("res://assets/sounds/player_hurt.wav")
 var use_sound : AudioStream = preload("res://assets/sounds/use_sound.wav")
+var dash_cooldown_timer : float = 0.0
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+	dash_cooldown_timer -= delta
 
 func distribute_signal(event : Event) -> void:
 	if event.type == event.types.take_damage:
@@ -11,6 +16,7 @@ func distribute_signal(event : Event) -> void:
 		AudioHandler.play_sound(use_sound, global_position)
 	elif event.type == event.types.inputcommand:
 		var input : InputCommand = event as InputCommand
-		if input.command == input.Commands.dash:
+		if input.command == input.Commands.dash and dash_cooldown_timer <= 0.01:
+			dash_cooldown_timer = 0.3
 			velocity += last_move_input * 200
 	super(event)
