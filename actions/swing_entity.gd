694036007e3f15e8d1be0c06c9c26c327cabf827
Daniel D <daniel.mi.do@gmail.com>
2024-03-16 18:04:09 +0000

    Swing actions now create new projectiles on swing

--- actions/swing_entity.gd
+++ actions/swing_entity.gd
@@ -6,8 +6,8 @@ extends Action
 @export var swing_angular_speed : float = 360
 @export var alternate : bool = true
 
-var entity : Entity
 var orbit_entity : Entity
+var entity : Entity
 
 var swinging : bool = false
 var start_angle : float
@@ -20,7 +20,7 @@ func move_to_orbit_location() -> void:
 	entity.global_position = orbit_entity.global_position + swing_distance * Vector2.from_angle(deg_to_rad(entity.rotation_degrees - 90))
 
 func _ready() -> void:
-	entity = entity_scene.instantiate() as Entity
+	var entity : Entity = entity_scene.instantiate() as Entity
 	description = "Swing " + a_or_an(entity.entity_name) + " in front of you"
 	expected_cooldown = swing_angle / swing_angular_speed
 
@@ -35,6 +35,8 @@ func _process(delta : float) -> void:
 
 func do(target : Entity, secondary : Entity = null, direction : Vector2 = Vector2.ZERO) -> void:
 	if not swinging:
+		entity = entity_scene.instantiate() as Entity
+		target.distribute_signal(CreatedProjectileEvent.new(entity))
 		orbit_entity = target
 		entity.team = orbit_entity.team
 		if alternate:
@@ -46,4 +48,4 @@ func do(target : Entity, secondary : Entity = null, direction : Vector2 = Vector
 		time_passed = 0
 		swing_time = swing_angle / swing_angular_speed
 		add_child(entity)
-		move_to_orbit_location()
+
