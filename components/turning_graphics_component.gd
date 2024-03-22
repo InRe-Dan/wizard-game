3178087f76ff7f0b71196c47d636c89ddf13fae8
Dan D. <daniel.mi.do@gmail.com>
2024-03-22 19:28:12 +0000

    Added an ice shard weapon and a new graphics component for it

---
+++ components/turning_graphics_component.gd
@@ -0,0 +1,14 @@
+extends EntityComponent
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotation = parent.velocity.angle()
+	pass
+
+func receive_signal(event : Event) -> Event:
+	return event
+
