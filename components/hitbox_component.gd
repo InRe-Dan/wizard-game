e0952b0a7f87eed0b49ec2ee1758891fb96c3f94
Daniel D <daniel.mi.do@gmail.com>
2024-04-12 11:04:18 +0100

    Fixed hitbox bug, capped FPS

--- components/hitbox_component.gd
+++ components/hitbox_component.gd
@@ -5,6 +5,8 @@ class_name HitboxComponent extends EntityComponent
 var entities_hit : Array[Entity]
 var immunity_durations : Array[float]
 
+var hitting : Array[Area2D]
+
 @export var damage : DamageData = DamageData.new()
 
 func _process(delta : float) -> void:
@@ -22,14 +24,13 @@ func _process(delta : float) -> void:
 			entities_hit.remove_at(i)
 			immunity_durations.remove_at(i)
 		i += 1
-	for child : Area2D in get_children():
-		for area : Area2D in child.get_overlapping_areas():
-			var entity_hit : Entity = area.get_parent().get_parent() as Entity
-			if not entity_hit:
-				push_error("Hitbox hit something weird!")
-			if entity_hit.team == (get_parent() as Entity).team and parent.team != EntityResource.EntityTeam.Any:
-				return
-			parent.distribute_signal(HasHitEvent.new(entity_hit, damage))
+	for area : Area2D in get_children().front().get_overlapping_areas():
+		var entity_hit : Entity = area.get_parent().get_parent() as Entity
+		if not entity_hit:
+			push_error("Hitbox hit something weird!")
+		if entity_hit.team == (get_parent() as Entity).team and parent.team != EntityResource.EntityTeam.Any:
+			continue
+		parent.distribute_signal(HasHitEvent.new(entity_hit, damage))
 
 func receive_signal(event : Event) -> Event:
 	match event.type:
@@ -41,3 +42,5 @@ func receive_signal(event : Event) -> Event:
 			entities_hit.append(hEvent.target)
 			immunity_durations.append(immunity_duration)
 	return event
+	
+
